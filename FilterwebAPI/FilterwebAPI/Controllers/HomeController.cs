2b7164e7fd896f0bde5583a08f1ff9725b16d4d3
unknown <mangay2000@gmail.com>
2021-07-20 15:05:03 +0530

    copied filter program

---
+++ FilterwebAPI/FilterwebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilterwebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HandleException]
+        public IActionResult Index()
+        {
+            throw new Exception("This is some exception!!!");
+           // return View();
+        }
+    }
+}
---
+++ FilterwebAPI/FilterwebAPI/MySampleACtionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilterwebAPI
+{
+    public class MySampleACtionFilter :Attribute, IActionFilter
+    {
+        private readonly string _name;
+        public MySampleACtionFilter(string name)
+        {
+            _name = name;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($"OnACtionExecuted - {_name}");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($"OnACtionExecuting- {_name}");
+        }
+    }
+    /* public class MySampleACtionFilter : IActionFilter
+     {
+         public void OnActionExecuted(ActionExecutedContext context)
+         {
+             Console.WriteLine("OnACtionExecuted");
+         }
+
+         public void OnActionExecuting(ActionExecutingContext context)
+         {
+             Console.WriteLine("OnACtionExecuting");
+         }
+     }*/
+}
